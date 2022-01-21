9a65f030507f2991317aaf1dcaf890e5803a33f5
maipadhega <vivekmahajan67@outlook.com>
2022-01-21 00:32:28 -0800

    testing creating queues

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     file = open('workwithqueues.py', 'r').read()
-    exec(file)
-    return "Hello, World!-By Vivek Mahajan. Another change. Trying to add another process"
+    return exec(file)
+   
