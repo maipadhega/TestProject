f7475a94a05894869e7cbee4635b248d9f05d4f5
maipadhega <vivekmahajan67@outlook.com>
2022-01-20 23:56:21 -0800

    adding queue stuff

---
+++ workwithqueues.py
@@ -0,0 +1,31 @@
+from azure.storage.queue import (
+        QueueClient,
+        BinaryBase64EncodePolicy,
+        BinaryBase64DecodePolicy
+)
+
+import os, uuid
+# Retrieve the connection string from an environment
+# variable named AZURE_STORAGE_CONNECTION_STRING
+connect_str = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
+
+# Create a unique name for the queue
+q_name = "queue-" + str(uuid.uuid4())
+
+# Instantiate a QueueClient object which will
+# be used to create and manipulate the queue
+print("Creating queue: " + q_name)
+queue_client = QueueClient.from_connection_string(connect_str, q_name)
+
+# Create the queue
+queue_client.create_queue()
+
+# Setup Base64 encoding and decoding functions
+base64_queue_client = QueueClient.from_connection_string(
+                            conn_str=connect_str, queue_name=q_name,
+                            message_encode_policy = BinaryBase64EncodePolicy(),
+                            message_decode_policy = BinaryBase64DecodePolicy()
+                        )
+message = u"Hello World"
+print("Adding message: " + message)
+queue_client.send_message(message)
