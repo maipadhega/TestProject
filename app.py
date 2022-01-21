c6984d2c1cf59efbd571c3a9ab02e5fa0bb6eaf1
maipadhega <vivekmahajan67@outlook.com>
2022-01-21 00:28:10 -0800

    invoking workwithqueues from flask

--- app.py
+++ app.py
@@ -3,4 +3,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World!-By Vivek Mahajan. Another change"
+    file = open('workwithqueues.py', 'r').read()
+    exec(file)
+    return "Hello, World!-By Vivek Mahajan. Another change. Trying to add another process"
